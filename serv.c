a9919220efeb6fd915ac98d1681089f0edc12756
Tashiana Johnson <tashiana.johnson.hu@gmail.com>
2024-10-21 21:23:37 +0000

    Cleaning up server output.

--- serv.c
+++ serv.c
@@ -12,21 +12,21 @@
 int main(int argc, char const* argv[]) 
 { 
   
-	  int n, val, idx;
+    int n, val, idx;
     // create server socket similar to what was done in 
     // client program 
     int servSockD = socket(AF_INET, SOCK_STREAM, 0); 
   
     // string store data to recv/send to/from client 
     char buf[1024];
-		char sbuf[1024];
+    char sbuf[1024];
     char* token;
 
     // define server address 
     struct sockaddr_in servAddr; 
   
 	  // list
-		list_t *mylist;
+    list_t *mylist;
 
 
     servAddr.sin_family = AF_INET; 
@@ -48,82 +48,78 @@ int main(int argc, char const* argv[])
     while(1){
         // recvs messages from client socket 
         n = recv(clientSocket, buf, sizeof(buf), 0);
-				buf[n] = '\0';
+        buf[n] = '\0';
 
         if(n > 0){
             token = strtok(buf, " ");
     
-						if(strcmp(token,"exit") == 0){
-                printf("client called exit!\n");
-							  list_free(mylist);
-							  exit(1);
-						}
-						else if(strcmp(token,"get_length") == 0){
-								// Make a Call
-								val = list_length(mylist);
-								sprintf(sbuf,"%s%d", "Length = ", val);
-                printf("client called get_length!\n");
-            }
-            else if(strcmp(token,"add_back") == 0){
-                token = strtok(NULL, " ");  // get next token (value)
+          if(strcmp(token,"exit") == 0){
+              list_free(mylist);
+              exit(1);
+          }
+          else if(strcmp(token,"get_length") == 0){
+              // Make a Call
+              val = list_length(mylist);
+              sprintf(sbuf,"%s%d", "Length = ", val);
+          }
+          else if(strcmp(token,"add_back") == 0){
+              token = strtok(NULL, " ");  // get next token (value)
+              val = atoi(token);
+              //  Make a call
+              sprintf(sbuf, "%s%d", ACK, val);
+              list_add_to_back(mylist, val);
+          }
+          else if(strcmp(token,"add_front") == 0){
+              token = strtok(NULL, " ");  // get next token (value)
+              val = atoi(token);
+              // Make a Call
+              sprintf(sbuf,"%s%d", ACK, val);
+              list_add_to_front(mylist,val);
+          }
+          else if (strcmp(token,"add_position") == 0){
+              token = strtok(NULL, " ");  // get next token (index)
+              idx = atoi(token);
+              token = strtok(NULL, " ");  // get next token (value)
+
+              //Make a Call
+              if (idx > list_length(mylist)){
+                sprintf(sbuf, "%s%d", ACK, val);                  
+              }
+              else{
                 val = atoi(token);
-                //  Make a call
                 sprintf(sbuf, "%s%d", ACK, val);
-                list_add_to_back(mylist, val);
-                printf("client called add_back\n");
-            }
-						else if(strcmp(token,"add_front") == 0){
-							  token = strtok(NULL, " ");  // get next token (value)
-								val = atoi(token);
-								// Make a Call
-								sprintf(sbuf,"%s%d", ACK, val);
-								list_add_to_front(mylist,val);
-                printf("client called add_front!\n");
-						}
-            else if (strcmp(token,"add_position") == 0){
-                token = strtok(NULL, " ");  // get next token (index)
-                idx = atoi(token);
-                token = strtok(NULL, " ");  // get next token (value)
+                list_add_at_index(mylist, val, idx);
+              }
+          }
+          else if (strcmp(token,"remove_back") == 0){
+              val = list_remove_from_back(mylist);
+              sprintf(sbuf, "%s%d", ACK, val);              
+          }
+          else if (strcmp(token,"remove_front") == 0){
+              val = list_remove_from_front(mylist);
+              sprintf(sbuf, "%s%d", ACK, val);
+          }
+          else if(strcmp(token,"remove_position") == 0){
+              token = strtok(NULL, " ");
+              idx = atoi(token);
+              // Make a Call
+              val = list_remove_at_index(mylist,idx);
+              sprintf(sbuf,"%s%d", ACK, val);
+          }
+          else if(strcmp(token,"get") == 0){
+              token = strtok(NULL, " ");
+              idx = atoi(token);
+              //  Make a Call
+              val = list_get_elem_at(mylist, idx);
+              sprintf(sbuf,"%s%d", ACK, val);
+          }
+          else if(strcmp(token,"print") == 0){
+              sprintf(sbuf,"%s", listToString(mylist));
 
-                //Make a Call
-                if (idx > list_length(mylist)){
-                  sprintf(sbuf, "%s%d", ACK, val);                  
-                }
-                else{
-                  val = atoi(token);
-                  sprintf(sbuf, "%s%d", ACK, val);
-                  list_add_at_index(mylist, val, idx);
-                }
-            }
-            else if (strcmp(token,"remove_back") == 0){
-                val = list_remove_from_back(mylist);
-                sprintf(sbuf, "%s%d", ACK, val);              
-            }
-            else if (strcmp(token,"remove_front") == 0){
-                val = list_remove_from_front(mylist);
-                sprintf(sbuf, "%s%d", ACK, val);
-            }
-            else if(strcmp(token,"remove_position") == 0){
-							  token = strtok(NULL, " ");
-								idx = atoi(token);
-								// Make a Call
-								val = list_remove_at_index(mylist,idx);
-								sprintf(sbuf,"%s%d", ACK, val);
-						}
-            else if(strcmp(token,"get") == 0){
-                token = strtok(NULL, " ");
-                idx = atoi(token);
-                //  Make a Call
-                val = list_get_elem_at(mylist, idx);
-                sprintf(sbuf,"%s%d", ACK, val);
-            }
-						else if(strcmp(token,"print") == 0){
-							  sprintf(sbuf,"%s", listToString(mylist));
+          }
 
-						}
-
-                // send's messages to client socket 
-            send(clientSocket, sbuf, sizeof(sbuf), 0);
+              // send's messages to client socket 
+          send(clientSocket, sbuf, sizeof(sbuf), 0);
         }
 				memset(buf, '\0', 1024);
     }
